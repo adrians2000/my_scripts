a53fcf3ed529556ab5019f5c0919cc2fc36e81c2
adrians2000 <adrians2000@users.noreply.github.com>
2016-11-13 18:17:06 +0200

    Update api.py

--- Rib/ansible/roles/api/files/api.py
+++ Rib/ansible/roles/api/files/api.py
@@ -9,13 +9,13 @@ def api():
     if "action" in request.args:
         action = request.args['action']
         if "start" in action:
-            return "This should start the Jenkins container \n"
+            return ("This should start the Jenkins container \n")
         elif "stop" in action:
-            return "This should stop the Jenkins container \n"
+            return ("This should stop the Jenkins container \n")
         else:
-            return "Not a valid action, chose from start/stop \n"
+            return ("Not a valid action, chose from start/stop \n")
     else:
-	return "API only for action, choices['start', 'stop'] \n"
+        return ("API only for action, choices['start', 'stop'] \n")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
